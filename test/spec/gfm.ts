ba8447a7b4a1aea1e4ac2b9abfad09cf041fd8e4
LinFeng1997 <244732635@qq.com>
2020-01-06 09:53:29 +0800

    chore：17 用例

--- test/spec/gfm.ts
+++ test/spec/gfm.ts
@@ -91,8 +91,11 @@ export default [
   },
   {
     index: 17,
-    md: ' ***\n  ***\n   ***\n\n',
+    md: '***\n***\n***\n\n',
     html: '<hr />\n<hr />\n<hr />\n\n',
+    option: {
+      hr: '***',
+    },
   },
   {
     index: 18,
