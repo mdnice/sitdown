9ecfa320a2a42448bb622f5c9420d386b66f0d5b
LinFeng1997 <244732635@qq.com>
2020-01-30 20:20:54 +0800

    docs:demo page

---
+++ .vuepress/components/Demo.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <button v-on:click="transform" class="transform">transform</button>
+        <div class="sitdown-demo">
+            <textarea v-model="input"/>
+            <textarea class="result"  disabled :value="result"/>
+        </div>
+    </div>
+</template>
+<script>
+    import {Sitdown} from '../../dist/src.esm';
+
+    export default {
+        data() {
+            return {
+                input: '',
+                result: ''
+            }
+        },
+        methods: {
+            transform() {
+                const result = this.sitdown.HTMLToMD(this.input);
+                this.result = result;
+            }
+        },
+        mounted() {
+            this.sitdown = new Sitdown({
+                keepFilter: ['style']
+            });
+            this.input = '';
+        }
+    }
+</script>
+<style lang="stylus">
+    .transform {
+        background-color: #4CAF50; /* Green */
+        border: none;
+        color: white;
+        padding: 10px 16px;
+        margin-bottom: 10px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+    }
+
+    .sitdown-demo {
+        display: flex;
+
+        textarea, .result {
+            width: 50%;
+            height: 600px;
+            padding 10px;
+        }
+    }
+</style>
--- .vuepress/nav/zh.js
+++ .vuepress/nav/zh.js
@@ -1,4 +1,5 @@
 module.exports = [
   {"link": "/zh-hans/示例/", "text": "示例"},
+  {"link": "/Demo.html", "text": "Demo"},
   {"link": "https://docschina.org/", "text": "印记中文"},
 ]
