c39d68cd22b079fe3d524cb05edbb67c38234617
LinFeng1997 <244732635@qq.com>
2020-02-02 18:19:50 +0800

    chore：梳理掘金转换工作量

--- test/juejin.test.ts
+++ test/juejin.test.ts
@@ -8,23 +8,29 @@ export const applyJuejinRule = (turndownService: TurndownService) => {
   turndownService.addRule('juejinImg', {
     filter: 'img',
 
-    replacement: function(_content:string, node) {
+    replacement: function(_content: string, node) {
       var alt = node.getAttribute('alt') || '';
       var src = node.getAttribute('data-src') || node.getAttribute('src') || '';
       var title = node.title || '';
       var titlePart = title ? ' "' + title + '"' : '';
-      return src ? '![' + alt + ']' + '(' + src + titlePart + ')' : ''
+      return src ? '![' + alt + ']' + '(' + src + titlePart + ')' : '';
     },
   });
 };
 
+/*
+掘金 html 转 md 存在的不一致处：
+1. 掘金进行了中文排版美化，如数字与中文、中文与英文间有空格
+2. 掘金对图片进行了转储，地址存储在 img 标签的 data-src 下
+3. 掘金对代码块做了处理，多了一个复制代码
+ */
 describe('juejin', () => {
   it('works', () => {
     let sitdown = new Sitdown({
       keepFilter: ['style'],
       codeBlockStyle: 'fenced',
       bulletListMarker: '-',
-      hr: '---'
+      hr: '---',
     });
     applyJuejinRule(sitdown.service);
     const expected = sitdown.HTMLToMD(html);
