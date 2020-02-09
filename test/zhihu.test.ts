78bd4fba5da9eee0dbed2ad3c5f9dfa2a40941a6
LinFeng1997 <244732635@qq.com>
2020-02-09 19:50:04 +0800

    test：知乎测试用例

--- test/zhihu.test.ts
+++ test/zhihu.test.ts
@@ -1,9 +1,9 @@
 import { Sitdown } from '../src';
-import md from './spec/markdownPaper/paper1.md';
+import md from './spec/markdownPaper/paper1-zhihu.md';
 import html from './spec/markdownPaper/paper1-zhihu.html';
-import md2 from './spec/markdownPaper/paper2.md';
+import md2 from './spec/markdownPaper/paper2-zhihu.md';
 import html2 from './spec/markdownPaper/paper2-zhihu.html';
-import md3 from './spec/markdownPaper/paper3.md';
+import md3 from './spec/markdownPaper/paper3-zhihu.md';
 import html3 from './spec/markdownPaper/paper3-zhihu.html';
 import TurndownService from '../src/lib/turndown';
 export const applyZhihuRule = (turndownService: TurndownService) => {
@@ -55,17 +55,16 @@ describe('知乎', () => {
   applyZhihuRule(sitdown.service);
   it('paper1 works', () => {
     const expected = sitdown.HTMLToMD(html);
-    require('fs').writeFileSync('xx.md', expected, 'utf-8');
     expect(expected).toEqual(md);
   });
 
   it('paper2 works', () => {
     const expected = sitdown.HTMLToMD(html2);
-    expect(expected).toEqual(md2);
+      expect(expected).toEqual(md2);
   });
 
   it('paper3 works', () => {
     const expected = sitdown.HTMLToMD(html3);
-    expect(expected).toEqual(md3);
+      expect(expected).toEqual(md3);
   });
 });
