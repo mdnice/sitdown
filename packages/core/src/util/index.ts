f17ba002cefdd5a11f327d74d976a260927ff861
LinFeng1997 <244732635@qq.com>
2020-02-17 11:20:29 +0800

    wip

---
+++ packages/core/src/util/index.ts
@@ -0,0 +1,13 @@
+export { escape } from './escape';
+export { isFence } from './isFence';
+export { isCode } from './isCode';
+export {
+  blankReplacement,
+  listReplacement,
+  fenceReplacement,
+  keepReplacement,
+} from './replacement';
+export { repeat } from './repeat';
+export { findParentNumber } from './findParentNumber';
+export { findOrderListIndentNumber } from './findOrderListIndentNumber';
+export { IndentCodeIsListfirstChild } from './indentCodeIsListfirstChild';
